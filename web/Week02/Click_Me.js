704fdf3470f4fd73ed2ac3f27f638afaca0b1fd2
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-01-14 16:37:44 -0700

    changes

---
+++ web/Week02/Click_Me.js
@@ -0,0 +1,11 @@
+function Clicked()
+{
+    alert("You clicked me!!!");
+}
+
+function Change_Color()
+{
+    var color = document.getElementById("color").value;
+    document.getElementById("div1").style.backgroundColor = color;
+    
+}
