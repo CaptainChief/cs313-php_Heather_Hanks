aedf8db71a59f7dada8f8f5ddf1d109688e278c7
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-01-14 16:49:30 -0700

    changes

--- web/Week02/Team_Activity02/Click_Me.js
+++ web/Week02/Team_Activity02/Click_Me.js
@@ -8,6 +8,7 @@ function Change_Color()
     var color = document.getElementById("color").value;
     alert(color);
     document.getElementById("div1").style.background = color;
+    document.getElementById("div1").className = "";
     document.getElementById("div1").style.color = "red";
     
 }
