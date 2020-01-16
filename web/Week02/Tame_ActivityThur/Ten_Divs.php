2575fc8753a864b7274e397f9b40429fd3ece574
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-01-16 15:48:07 -0700

    TAThurs

--- web/Week02/Tame_ActivityThur/Ten_Divs.php
+++ web/Week02/Tame_ActivityThur/Ten_Divs.php
@@ -3,14 +3,23 @@
 <head>
     <title>Week 2 Team Assignment</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="personal.css">
 </head>
 <body>
 
 <div>
+    <h1>PHP Team Activity</h1>
     <?php
       for($i = 0; $i < 10; $i++)
       {
-          echo "<div><p>This is div #$i</p></div>";
+          if ($i % 2 == 0) 
+          {
+              echo "<div class='red'><p>This is div #$i</p></div>";
+          }
+          else
+          {
+              echo "<div><p>This is div #$i</p></div>";
+          }
       }
     ?>
 </div>
