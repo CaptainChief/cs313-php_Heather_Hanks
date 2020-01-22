01aee68f82bd667ffede1b7ee38739f406cfd4d9
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-01-21 17:08:55 -0700

    Start up Shopping List

---
+++ web/Week03/Shopping_List/browse_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Week 3 Team Assignment</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="personal.css">
+</head>
+
+<body>
+
+  <ul>
+    <li><a class="active" href="">Browse Items</a></li>
+    <li><a href="view_cart.php">View Cart</a></li>
+  </ul>
+ 
+
+</body>
+
+
+</html>
---
+++ web/Week03/Shopping_List/view_cart.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Week 3 Team Assignment</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="personal.css">
+</head>
+
+<body>
+
+  <ul>
+    <li><a href="brows_page.php">Browse Books</a></li>
+    <li><a class="active" href="">View Cart</a></li>
+  </ul>
+ 
+
+</body>
+
+
+</html>
