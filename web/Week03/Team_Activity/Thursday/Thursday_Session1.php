f2d0b53262675ca1dc9cdc806ea44afcbbc3bee8
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-01-23 16:00:32 -0700

    Testing items.

--- web/Week03/Team_Activity/Thursday/Thursday_Session1.php
+++ web/Week03/Team_Activity/Thursday/Thursday_Session1.php
@@ -1,5 +1,7 @@
 <?php
    // Start the session
+   session_start();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,10 +9,17 @@
       <?php
          // remove previous session variable
          // Set session variables
+         $_SESSION["favorite_color"] = "Blue";
+         $_SESSION["favorite_animal"] = "Wolves";
+         $_SESSION["favorite_game"];
          // echo that variables have been set
-?>
+         echo "Session variables have been set.";
+      ?>
+
       <a href="Thursday_Session2.php">Check the variables on another page</a>
 
-      <?php // set session variables using a form ?>
+      <?php // set session variables using a form 
+
+      ?>
    </body>
 </html>
