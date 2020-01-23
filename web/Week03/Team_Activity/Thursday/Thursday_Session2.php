8ba38fcc96a579722d09c83f747447899f621f1e
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-01-23 16:04:49 -0700

    Thursday stuff

--- web/Week03/Team_Activity/Thursday/Thursday_Session2.php
+++ web/Week03/Team_Activity/Thursday/Thursday_Session2.php
@@ -1,6 +1,11 @@
 <?php 
    // start session
+   session_start();
    // save session variables into local variables
+   $fave_animal = $_SESSION["favorite_animal"];
+   $fave_color = $_SESSION["favorite_color"];
 ?>
-<h1><?php // use the session variables ?></h1>
+<h1><?php // use the session variables 
+        echo "Your favorite animal is: " . $fave_animal . "<br>"; 
+        echo "And your favorite color is: " . $fave_color . "<br>"; ?></h1>
 <?php ?>
