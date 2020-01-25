5b51bfe261132bec31d576f7ff0aaf014c845857
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-01-24 19:58:22 -0700

    Shopping List testing

--- web/Week03/Shopping_List/browse_page.php
+++ web/Week03/Shopping_List/browse_page.php
@@ -56,23 +56,20 @@
 
       <div>
         Percy Jackson: Sea of Monsters - Rick Riordan<br>
-        <input type = 'button' name = 'PJ_2' onClick="add('PJ_2')" value = "Add to Cart"> <br><hr>
+        <input type = 'button' name = 'PJ_2' value = "Add to Cart"> <br><hr>
       </div>
       
       <div>
         Covenants - Lorna Freeman<br>
-        <input type = 'button' name = 'Cov' onClick="add('Cov')" value = "Add to Cart"> <br><hr>
+        <input type = 'button' name = 'Cov' value = "Add to Cart"> <br><hr>
       </div>
       
       <div>
         Ransom - Julie Garwood<br>
-        <input type = 'button' name = 'Ran' onClick="add('Ran')" value = "Add to Cart"> <br><hr>
+        <input type = 'button' name = 'Ran' value = "Add to Cart"> <br><hr>
       </div>
 
-  </div>
-
-    <br><br><br>
-      
+  </div>      
   </div>    
   </div>
 </body>
