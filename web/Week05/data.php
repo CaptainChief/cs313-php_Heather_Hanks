9cb8d7d746aef9963a06538038b81d4259effd57
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-04 16:26:58 -0700

    Tuesday Example

--- web/Week05/data.php
+++ web/Week05/data.php
@@ -2,7 +2,13 @@
     require "dbConnect.php";
     $db = get_db();
 ?>
-
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>TA05</title>
+</head>
+<body>
 
 <form action="data.php" method="post">
     <select name="Scripture">
@@ -13,22 +19,25 @@
 
     <input type = "submit" value = "Search">
 </form>
-
 <hr>
 
 <?php
-   if (isset($_POST['Scripture'])) {
-    $scr = $db->prepare("SELECT * FROM scriptures WHERE book = $_POST['Scripture']");
-    $scr->execute();
+   if (isset($_POST['Scripture'])) 
+   {
+      $scr = $db->prepare("SELECT * FROM scriptures WHERE book = $_POST['Scripture']");
+      $scr->execute();
 
-    echo "<h1> <b>SCRIPTURE RESOURCES</b> </h1>";
-    while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
-    {
-        $book = $frow["book"];
-        $chapter = $frow["chapter"];
-        $verse = $frow["verse"];
-        $content = $frow["content"];
+      echo "<h1> <b>SCRIPTURE RESOURCES</b> </h1>";
+      while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
+      {
+         $book = $frow["book"];
+         $chapter = $frow["chapter"];
+         $verse = $frow["verse"];
+         $content = $frow["content"];
         echo "<p><b>$book $chapter:$verse</b> - \"$content\"</p><br>";
-    }
+      }
    }
-?>
+   ?>
+
+</body>
+</html>
