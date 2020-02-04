8331c20469c98447e142527aa44c942854be1c92
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-04 16:31:43 -0700

    Tuesday Example

--- web/Week05/data.php
+++ web/Week05/data.php
@@ -25,13 +25,13 @@
    if (isset($_POST['Scripture'])) 
    {
       $book_name = $_POST['Scripture'];
-      echo $book_name;
       $scr = $db->prepare("SELECT * FROM scriptures WHERE book = $book_name");
       $scr->execute();
 
       echo "<h1> <b>SCRIPTURE RESOURCES</b> </h1>";
       while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
       {
+          echo "asjdfl;alskdfj     in the loop";
          $book = $frow["book"];
          $chapter = $frow["chapter"];
          $verse = $frow["verse"];
