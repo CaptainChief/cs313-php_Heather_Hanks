39960d7f3f41a6426b876582fb12e4d5d16ff2cf
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-04 16:15:26 -0700

    Tuesday Example

---
+++ web/Week05/scripture.php
@@ -0,0 +1,19 @@
+<?php 
+    require "dbConnect.php";
+    $db = get_db();
+
+    $scr = $db->prepare("SELECT * FROM scriptures");
+    $scr->execute();
+
+    while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
+    {
+        $book = $frow["book"];
+        $chapter = $frow["chapter"];
+        $verse = $frow["verse"];
+        $content = $frow["content"];
+
+        echo "<h1> <b>SCRIPTURE RESOURCES</b> </h1>";
+        echo "<p><b>$book $chapter:$verse</b> - $content</p><br>";
+    }
+
+?>
