ce759a8fe7be9e655b30b9f663fd4f8cd1d60756
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-05 12:48:09 -0700

    Creation of pages for php project

---
+++ web/PHP_Project/create_species.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Week 2 Team Assignment</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="personal.css">
+</head>
+
+<body>
+
+<ul>
+    <li><a href="main_page.php">Home Page</a></li>
+    <li><a href="create_habitat.php">Create Habitat</a></li>
+    <li><a href="create_location.php">Create Location</a></li>
+    <li><a class="active" href="">Create Species</a></li>
+    <li><a href="create_genus.php">Create Genus</a></li>
+  </ul>
+
+  <div class = "center">
+    <img src="construction_image.webp" alt="Construction Site">
+  </div>
+
+</body>
+
+
+</html>
