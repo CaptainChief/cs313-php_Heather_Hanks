bf3dd693199d7d97efb956a92fe43175eeb0ca19
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-05 13:05:53 -0700

    Creation of pages for php project

--- web/PHP_Project/main_page.php
+++ web/PHP_Project/main_page.php
@@ -9,13 +9,35 @@
 
 <body>
 
-  <ul>
+<div class="navbar">
+  <a class="active" href="">Home Page (View Data)</a>
+  <div class="subnav">
+    <button class="subnavbtn active">Home Page (View Data) <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="">Animals</a>
+      <a href="">Locations</a>
+      <a href="">Habitats</a>
+    </div>
+  </div>
+  <div class="subnav">
+    <button class="subnavbtn">Create <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="create_habitat.php">Create Habitat</a>
+      <a href="create_location.php">Create Location</a>
+      <a href="create_species.php">Create Species</a>
+      <a href="create_genus.php">Create Genus</a>
+    </div>
+  </div>
+</div>
+
+
+  <!-- <ul>
     <li><a class="active" href="">Home Page</a></li>
     <li><a href="create_habitat.php">Create Habitat</a></li>
     <li><a href="create_location.php">Create Location</a></li>
     <li><a href="create_species.php">Create Species</a></li>
     <li><a href="create_genus.php">Create Genus</a></li>
-  </ul>
+  </ul> -->
 
 
 
