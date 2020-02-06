405e2b7985e9fa38f1e1be821543168a38eefeca
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-06 16:31:54 -0700

    Thursday ACtivity

--- web/Week05/thurs.php
+++ web/Week05/thurs.php
@@ -4,7 +4,7 @@
 
     echo "<h1>These are the events</h1><br><hr>"
 
-    $scr = $db->prepare("SELECT name, image FROM w5_EVENT");
+    $scr = $db->prepare("SELECT name FROM w5_EVENT");
     $scr->execute();
     // $i = 1;
     // while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
