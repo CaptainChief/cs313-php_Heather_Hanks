9a40e1a368a1b2669d13078af7efcf35a7467c55
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-11 16:20:52 -0700

    Create log in page

--- web/Week02/Week02_Assignment/index_page.php
+++ web/Week02/Week02_Assignment/index_page.php
@@ -19,7 +19,7 @@
   <div class = "center">
     <a href="..\..\Week03\Shopping_List\browse_page.php">Assignment 03: Shopping List</a><br>
     <a href="..\..\PHP_Project\log_in.php">PHP Project</a><br>
-    <a href="..\..\Week06\team_activity\index.php">Week 06</a>
+    <a href="..\..\Week06\team_activity\display.php">Week 06</a>
   </div>
 
   <!-- <div class = "center">
