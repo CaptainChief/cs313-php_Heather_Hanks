4c9d0efe293ed640c34bd6a1ea6fbde7aabdaca9
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-11 15:13:32 -0700

    Create log in page

---
+++ web/PHP_Project/log_in.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Week 2 Team Assignment</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="personal.css">
+</head>
+
+<body>
+
+<div class="headerLogo center"></div>
+
+
+
+  <div class = "center">
+    First name:<br>
+    <input type="text" name="firstname"><br>
+    Password:<br>
+    <input type="password" name="pass"><br><br>
+    <input type="button" name="new_user" value="Create New User"><br><br>
+  </div>
+
+</body>
+
+
+</html>
