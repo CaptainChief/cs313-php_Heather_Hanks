d8566d070fd29e1587fcd76eaa68eaf87ba62f3e
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-11 15:43:27 -0700

    Create log in page

--- web/Week06/team_activity/display.php
+++ web/Week06/team_activity/display.php
@@ -5,6 +5,29 @@
 	<body>
 		<div class="container">
          <?php
+            $personid = $_GET['personId'];
+
+            $statement = $db->prepare('SELECT * FROM w6_user WHERE ID = :personid');
+            $statement->bindValue(':personid', $personid);
+            $statement->execute();
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+               $id = $row['id'];
+               $first = $row['first_name'];
+               $last = $row['last_name'];
+               $food_id = $row['food_type'];
+
+               $statement = $db->prepare('SELECT food FROM w6_food WHERE ID = :food_id');
+               $statement->bindValue(':food_id', $food_id);
+               $statement->execute();
+               while ($frow = $statement->fetch(PDO::FETCH_ASSOC))
+               {
+                  $food = $frow['food'];
+               }
+
+               echo "<h1>$first $last's favorite food is $food</h1>";
+
+            }
             // retrieve url parameter
             // execute query to pull up data from that id
             // execute another query to get food data
