8329ff14b4fd85cd8833f261c19b864a4abd0e70
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-12 13:24:28 -0700

    create_specie page

--- web/PHP_Project/create_species.php
+++ web/PHP_Project/create_species.php
@@ -20,7 +20,32 @@
   </ul>
 
   <div class = "center">
-    <img src="construction_image.webp" alt="Construction Site">
+  <form>
+    Specie Name: <input type="text" id="specie_name">
+    Specie Def : <br> 
+    <textarea id="specie_def" rows="4" cols="50">
+    </textarea>
+
+    <label for="genus">What genus is the specie from?</label>
+    <select id="genus">
+    <?php
+      $scr1 = $db->prepare("SELECT g.genus_id, g.genus_name
+                            FROM animal_genus g");
+      
+      $scr1->execute();
+      while ($frow = $scr1->fetch(PDO::FETCH_ASSOC))
+      {
+        $g_id = $frow["genus_id"];
+        $g_name = $frow["genus_name"];
+
+        echo "<option value='$g_id'>$g_name</option>";
+      }
+
+    ?>
+    </select>
+
+    
+  </form>
   </div>
 
 </body>
