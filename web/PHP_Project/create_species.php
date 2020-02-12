98634904c0d7a022d6cdc0f1a3919d5c390411f2
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-12 13:43:59 -0700

    create_specie page

--- web/PHP_Project/create_species.php
+++ web/PHP_Project/create_species.php
@@ -1,3 +1,8 @@
+<?php
+   require("dbConnect.php");
+   $db = get_db();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,19 +35,19 @@
 
     <select id="genus">
     <?php
-      // $scr = $db->prepare("SELECT genus_id, genus_name
-      //                       FROM animal_genus
-      //                       ORDER BY genus_name ASC");
+      $scr = $db->prepare("SELECT genus_id, genus_name
+                            FROM animal_genus
+                            ORDER BY genus_name ASC");
       
-      // $scr->execute();
-      // while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
-      // {
-      //   $g_id = $frow["genus_id"];
-      //   $g_name = $frow["genus_name"];
+      $scr->execute();
+      while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
+      {
+        $g_id = $frow["genus_id"];
+        $g_name = $frow["genus_name"];
 
-      //   echo "<option value='$g_id'>$g_name</option>";
-      //   echo "Through the looop at least once";
-      // }
+        echo "<option value='$g_id'>$g_name</option>";
+        echo "Through the looop at least once";
+      }
 
     ?> 
     </select><br><br>
