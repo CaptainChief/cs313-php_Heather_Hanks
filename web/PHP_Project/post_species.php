f25233271c8ff265e24c567fc1ce86a3448fbe34
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-13 15:12:35 -0700

    Create Specie Page

--- web/PHP_Project/post_species.php
+++ web/PHP_Project/post_species.php
@@ -5,8 +5,11 @@
     $name = $_POST["specie_name"];
     $def = $_POST["specie_def"];
     $g_name = $_POST["genus"];
+    $habitats = $_POST["habitats"];
+    $locations = $_POST["locations"];
 
-    echo "The name of the genus is: $g_name";
+    echo "The name of the genus is: $habitats";
+    echo "The name of the locations are: $locations"
 
 
     //Filter
