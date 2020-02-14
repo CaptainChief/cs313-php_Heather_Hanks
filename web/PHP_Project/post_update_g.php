8b2fa71ee48c1396a2d69fd0040c38288d9b1d2e
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-13 20:12:52 -0700

    Update page

--- web/PHP_Project/post_update_g.php
+++ web/PHP_Project/post_update_g.php
@@ -6,11 +6,16 @@
    $def = $_POST["genus_def"];   //specie
    $g_id = $_POST["g_id"];       //specie
 
-   echo "this is $g_id";
+    //Filter
+    $name = htmlspecialchars($name);
+    $def = htmlspecialchars($def);
 
+    $query =("UPDATE animal_genus
+              SET genus_name = :name, genus_def = :def
+              WHERE genus_id = $g_id;");
+    $statement = $db->prepare($query);
+	$statement->bindValue(':name', $name); //This will help keep statements safe
+	$statement->bindValue(':def', $def);
 
-   
-
-
-
+    $scr->execute();
 ?>
