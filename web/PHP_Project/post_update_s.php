ca6f8e055449251098c7be1d08bd82f2b72153d5
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-14 19:46:55 -0700

    Finish update page

--- web/PHP_Project/post_update_s.php
+++ web/PHP_Project/post_update_s.php
@@ -29,13 +29,13 @@
 
         $scr->execute();
 
-        // $frow = $statement->fetch(PDO::FETCH_ASSOC);
-        // if(!$frow)
-        // {
-            // $query = "INSERT INTO species_and_habitats (specie_id, habitat_id) VALUES($s_id, $habitat)";
-            // $statement = $db->prepare($query);
-            // $statement->execute();
-        // }
+        $frow = $scr->fetch(PDO::FETCH_ASSOC);
+        if(!$frow)
+        {
+            $query = "INSERT INTO species_and_habitats (specie_id, habitat_id) VALUES($s_id, $habitat)";
+            $statement = $db->prepare($query);
+            $statement->execute();
+        }
 
     }
     // foreach($locations as $location)
