ea58c278f24051a53d815792d0d113298fda3ef9
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-14 20:13:54 -0700

    Finish update page

--- web/PHP_Project/post_update_s.php
+++ web/PHP_Project/post_update_s.php
@@ -13,14 +13,16 @@
     $name = htmlspecialchars($name);
     $def = htmlspecialchars($def);
 
-    // $query =("UPDATE animal_species
-    //           SET specie_name = :name, specie_def = :def, genus_id = $g_id
-    //           WHERE specie_id = $s_id;");
-    // $statement = $db->prepare($query);
-	// $statement->bindValue(':name', $name); //This will help keep statements safe
-	// $statement->bindValue(':def', $def);
+    $query =("UPDATE animal_species
+              SET specie_name = :name, specie_def = :def, genus_id = $g_id
+              WHERE specie_id = $s_id;");
+    $statement = $db->prepare($query);
+	$statement->bindValue(':name', $name); //This will help keep statements safe
+	$statement->bindValue(':def', $def);
 
-    // $statement->execute();
+    $statement->execute();
+
+    //Check and add any new habitats
     foreach($habitats as $habitat)
     {
         $scr = $db->prepare("SELECT habitat_id
@@ -42,8 +44,9 @@
                          WHERE specie_id = $s_id");
 
     $scr->execute();
+
+    //Check and remove any habitats the user did not check.
     $delete = 1;
-    $habitat = -1;
 
     while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
     {
@@ -51,7 +54,6 @@
 
         foreach($habitats as $habitat)
         {
-            echo "Does $habitat == $h_id?<br><br>";
             if($habitat == $h_id)
             {
                 $delete = 0;
@@ -62,30 +64,64 @@
                 $delete = 1;
             }
         }
-
         if($delete)
         {
             $scr1 = $db->prepare("DELETE FROM species_and_habitats
-                                WHERE specie_id = $s_id AND habitat_id = $h_id");
+                                  WHERE specie_id = $s_id AND habitat_id = $h_id");
             $scr1->execute();
         }
-
     }
-    // foreach($locations as $location)
-    // {
-    //     $scr = $db->prepare("SELECT l.location_id, location_name
-    //                         FROM locations
-    //                         ORDER BY location_name ASC");
-      
-    //     $scr->execute();
-    //     while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
-    //     {
 
-    //         $g_id = $frow["location_id"];
-    //         $g_name = $frow["location_name"];
-                
-    //     }
-    // }
+
+    //Check and add any new locations
+    foreach($locations as $location)
+    {
+        $scr = $db->prepare("SELECT location_id
+                            FROM species_and_location
+                            WHERE location_id = $location AND specie_id = $s_id");
+    
+        $scr->execute();
+    
+        $frow = $scr->fetch(PDO::FETCH_ASSOC);
+        if(!$frow)
+        {
+            $query = "INSERT INTO species_and_location (specie_id, location_id) VALUES($s_id, $location)";
+            $statement = $db->prepare($query);
+            $statement->execute();
+        }
+    }
+    $scr = $db->prepare("SELECT location_id
+                        FROM species_and_location
+                        WHERE specie_id = $s_id");
+    
+    $scr->execute();
+    
+    //Check and remove any locations the user did not check.
+    $delete = 1;
+    
+    while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
+    {
+        $l_id = $frow["location_id"];
+    
+        foreach($locations as $location)
+        {
+            if($location == $l_id)
+            {
+                $delete = 0;
+                break;
+            }
+            else
+            {
+                $delete = 1;
+            }
+        }
+        if($delete)
+        {
+            $scr1 = $db->prepare("DELETE FROM species_and_location
+                                WHERE specie_id = $s_id AND location_id = $l_id");
+            $scr1->execute();
+        }
+    }
 
     // header('Location: main_page.php');
 ?>
