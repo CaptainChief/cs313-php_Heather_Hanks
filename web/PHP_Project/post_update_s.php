05cd57678ba39b17f11cac1dc5e2ba756b88de14
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-14 19:22:53 -0700

    Finish update page

--- web/PHP_Project/post_update_s.php
+++ web/PHP_Project/post_update_s.php
@@ -9,18 +9,25 @@
    $habitats = $_POST["habitats"]; //specie_habitat
    $locations = $_POST["locations"]; //specie_location
 
+   echo "name=$name<br>";
+   echo "def=$def<br>";
+   echo "g_id=$g_id<br>";
+   echo "s_id=$s_id<br>";
+   echo "habitats=$habitats<br>";
+   echo "locations=$locations<br>";
+
     //Filter
     $name = htmlspecialchars($name);
     $def = htmlspecialchars($def);
 
-    $query =("UPDATE animal_species
-              SET specie_name = :name, specie_def = :def
-              WHERE specie_id = $s_id;");
-    $statement = $db->prepare($query);
-	$statement->bindValue(':name', $name); //This will help keep statements safe
-	$statement->bindValue(':def', $def);
+    // $query =("UPDATE animal_species
+    //           SET specie_name = :name, specie_def = :def
+    //           WHERE specie_id = $s_id;");
+    // $statement = $db->prepare($query);
+	// $statement->bindValue(':name', $name); //This will help keep statements safe
+	// $statement->bindValue(':def', $def);
 
-    $statement->execute();
+    // $statement->execute();
 
     header('Location: main_page.php');
 ?>
--- web/PHP_Project/update_specie.php
+++ web/PHP_Project/update_specie.php
@@ -92,26 +92,8 @@
 
                 $g_id = $frow["location_id"];
                 $g_name = $frow["location_name"];
-                $script = $db->prepare("SELECT l.location_name
-                                        FROM species_and_location sl
-                                        JOIN location l
-                                        ON sl.location_id = l.location_id
-                                        WHERE l.specie_id = $id");
-
-                $script->execute();
-                while ($row = $scr->fetch(PDO::FETCH_ASSOC))
-                {
-                    $sl_name = $row["location_name"];
-                    if($sl_name == $g_name)
-                    {
-                        echo "<input type='checkbox' name='locations[]' value='$g_id' checked>$g_name<br>";
-                        $g_name = 'skip';
-                    }
-                }
-                if($g_name != 'skip')
-                {
-                    echo "<input type='checkbox' name='locations[]' value='$g_id'>$g_name<br>";
-                }
+                
+                echo "<input type='checkbox' name='locations[]' value='$g_id'>$g_name<br>";
                 
             }
 
