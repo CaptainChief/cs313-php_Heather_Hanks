991384d990522ee6272aaac812b8aef3cc2aad55
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-14 19:32:04 -0700

    Finish update page

--- web/PHP_Project/post_update_s.php
+++ web/PHP_Project/post_update_s.php
@@ -9,25 +9,51 @@
    $habitats = $_POST["habitats"]; //specie_habitat
    $locations = $_POST["locations"]; //specie_location
 
-   echo "name=$name<br>";
-   echo "def=$def<br>";
-   echo "g_id=$g_id<br>";
-   echo "s_id=$s_id<br>";
-   echo "habitats=$habitats<br>";
-   echo "locations=$locations<br>";
+   foreach($locations as $location)
+   {
+       echo "location=$location";
+   }
 
     //Filter
     $name = htmlspecialchars($name);
     $def = htmlspecialchars($def);
 
     // $query =("UPDATE animal_species
-    //           SET specie_name = :name, specie_def = :def
+    //           SET specie_name = :name, specie_def = :def, genus_id = $g_id
     //           WHERE specie_id = $s_id;");
     // $statement = $db->prepare($query);
 	// $statement->bindValue(':name', $name); //This will help keep statements safe
 	// $statement->bindValue(':def', $def);
 
     // $statement->execute();
+    // foreach($habitats as $habitat)
+    // {
+    //     $scr = $db->prepare("SELECT habitat_id, habitat_name
+    //     FROM habitats
+    //     ORDER BY habitat_name ASC");
 
-    // header('Location: main_page.php');
+    //     $scr->execute();
+    //     while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
+    //     {
+    //         $g_id = $frow["habitat_id"];
+    //         $g_name = $frow["habitat_name"];
+    //     }
+    // }
+    // foreach($locations as $location)
+    // {
+    //     $scr = $db->prepare("SELECT l.location_id, location_name
+    //                         FROM locations
+    //                         ORDER BY location_name ASC");
+      
+    //     $scr->execute();
+    //     while ($frow = $scr->fetch(PDO::FETCH_ASSOC))
+    //     {
+
+    //         $g_id = $frow["location_id"];
+    //         $g_name = $frow["location_name"];
+                
+    //     }
+    // }
+
+    header('Location: main_page.php');
 ?>
