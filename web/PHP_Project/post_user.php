cc9c471070d70155e85e6175830cab7a610a3a53
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-02-20 17:45:08 -0700

    Create log in page

--- web/PHP_Project/post_user.php
+++ web/PHP_Project/post_user.php
@@ -15,19 +15,19 @@
    $retrieveStatement->bindValue(':name', $name);
    $retrieveStatement->execute();
 
-   $row = $retrieveStatement->fetch(PDO::FETCH_ASSOC);
-   if (isset($row['creator_name'])) {
-      $_SESSION['errorStr'] = "Username already taken";
-      $url = 'creat_user.php';
-      header('Location: ' . $url);
-      die();
-   }
+   // $row = $retrieveStatement->fetch(PDO::FETCH_ASSOC);
+   // if (isset($row['creator_name'])) {
+   //    $_SESSION['errorStr'] = "Username already taken";
+   //    $url = 'creat_user.php';
+   //    header('Location: ' . $url);
+   //    die();
+   // }
 
-   $inserStatement = $db->prepare("INSERT INTO creators (creator_name, creator_pass) VALUES(:name, :hash)");
-   $inserStatement->bindValue(':name', $name);
-   $inserStatement->bindValue(':hash', $hash);
-   $inserStatement->execute();
+   // $inserStatement = $db->prepare("INSERT INTO creators (creator_name, creator_pass) VALUES(:name, :hash)");
+   // $inserStatement->bindValue(':name', $name);
+   // $inserStatement->bindValue(':hash', $hash);
+   // $inserStatement->execute();
 
-   header('Location: ' . $url);
-   die();
+   // header('Location: ' . $url);
+   // die();
 ?>
